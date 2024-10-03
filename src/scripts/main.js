d55226ff39fbc7458d94936017c40832298b4482
adityapatel579 <aditya.patel@innovationcharter.org>
2024-10-03 13:28:38 +0000

    got step 2 done and partially step 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,14 @@
 import "../styles/styles.css";
 // Add imports here
-
+import Aos from "aos";
+import "aos/dist/aos.css";
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
   // Put code that runs ONLY in the browser here -- this is most likely where
   // MOST of your code should go.
   //
+  Aos.init();
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
 }
